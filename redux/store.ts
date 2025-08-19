1cc537ba27eb62a0985c9834d6f474a18ba66b88
Muhammadamin Mamadaliyev <91719420+mamadaliyev66@users.noreply.github.com>
2025-08-19 13:36:03 -0700

    fix: silence persist serializable warning

--- redux/store.ts
+++ redux/store.ts
@@ -1,6 +1,15 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { persistReducer, persistStore } from 'redux-persist';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import auth from './slices/authSlice';
 import menu from './slices/menuSlice';
@@ -30,6 +39,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
